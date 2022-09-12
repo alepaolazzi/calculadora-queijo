df087418488810578b6fb687702434237b9c03eb
alepaolazzi <alepaolazzidasilva@gmail.com>
2022-09-12 18:34:11 -0300

    adicionando botão para apagar um número

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ const numeros = document.querySelectorAll(".numeros button");
 const peso = document.querySelector("#peso");
 const calcular = document.querySelector("#calcular");
 const resultado = document.querySelector("#resultado");
+const apagar = document.querySelector("#apagar");
 
 let num = 0;
 
@@ -17,6 +18,7 @@ function recebeNumero(e) {
 
 function realizaCalculo() {
   calcular.setAttribute("disabled", "");
+  apagar.setAttribute("disabled", "");
   if (peso.value.includes(".")) {
     num = parseFloat(peso.value) * 18;
     console.log(num);
@@ -31,11 +33,17 @@ function realizaCalculo() {
   }
 }
 
+function removeUltimo() {
+  peso.value = peso.value.substring(0, peso.value.length - 1);
+  console.log(peso.value);
+}
+
 function clear() {
   peso.value = "";
   resultado.innerText = "\u00A0";
   num = 0;
   calcular.removeAttribute("disabled");
+  apagar.removeAttribute("disabled");
 }
 
 numeros.forEach((e) => {
@@ -43,3 +51,5 @@ numeros.forEach((e) => {
 });
 
 calcular.addEventListener("click", realizaCalculo);
+
+apagar.addEventListener("click", removeUltimo);
